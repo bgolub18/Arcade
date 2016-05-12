6907695cbfa1f1336ac86657667374b79b4015ff
Ben Golub <bgolub@lsoc.org>
2016-05-12 13:17:01 -0500

    Successful RPS

--- Controller.rb
+++ Controller.rb
@@ -3,15 +3,34 @@ require_relative 'Models/rps.rb'
 enable :sessions
 
 get '/' do
+	session[:wins] ||= 0
+	session[:loses] ||= 0
 	session[:game]= Game.new
-	session[:wins] = @wins
-	session[:loses] = @loses
 	erb :index
 end
 
 post '/' do
 	game = session[:game] 
 	@game = game.play(params[:choice])
-	erb :results
+	if @game == "You won"
+		session[:wins] = session[:wins] + 1
+	elsif @game == "You lost"
+		session[:loses] = session[:loses] + 1
+	end 	
+	@score = "#{session[:wins]} - #{session[:loses]}"
+	if session[:wins] == 2
+		@end = "You won the round of 3!"
+		session[:wins] = 0
+		session[:loses] = 0
+		erb :gameover
+	elsif session[:loses] == 2
+		@end = "You lost the round of 3."
+		session[:wins] = 0
+		session[:loses] = 0
+		erb :gameover
+	else
+		@end = nil	
+		erb :results
+	end
 end
 
